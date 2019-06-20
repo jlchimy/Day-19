4cf571c9c34a683547a796c23bf70e08e78a74c2
aku8 <aninaku@Aninas-MBP.lan>
2019-06-20 11:02:46 +0100

    working login authentication

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
+import axios from "axios";
 import './App.css';
 
-function App() {
+
+class Login extends Component{ 
+constructor(props){
+  super(props);
+  this.state={
+  username:'',
+  password:''
+  }
+ 
+
+ }
+
+
+render() {
   return (
     <div className="App">
       <header className="App-header">
@@ -12,14 +26,20 @@ function App() {
         </p>
         <div>
           Enter your email:
-          <input type="text" placeholder="Email"></input>
+          <input type="text" onChange = {(event,newValue) => {
+            console.log(event.target.value, newValue)
+            this.setState({email:event.target.value});
+          }} placeholder="Email"></input>
         </div>
         <div>
           Enter your password:
-          <input type="password" placeholder="Password"></input>
+          <input type="password" onChange = {(event, newValue) => {
+            console.log(event.target.value, newValue)
+            this.setState({password: event.target.value});}}
+            placeholder="Password"></input>
         </div>
         <div>
-          <button onClick="login()">
+          <button onClick={(event) => this.login()}>
             Login
           </button>
         </div>
@@ -27,9 +47,34 @@ function App() {
     </div>
   );
 }
+login() {
+  var apiBaseUrl = "http://localhost:4000/";
+  var self = this;
+  var inputs = {
+    "email": this.state.email,
+    "password": this.state.password
+  }
+  console.log(inputs);
 
-function login() {
+  axios.post(apiBaseUrl+'users/authentication', inputs)
+  .then(function (response) {
+    console.log(response);
+
+
+    if(response.status === 200) {
+      console.log("Login successful!");
+    }
+  })
+
+  
+  .catch(function (error) {
+    const response = error.response;
+    if (response.status === 401) {
+      console.log("Username password do not match.")
+      alert("Invalid login!")
+    }
+  });
   
 }
-
-export default App;
+}
+export default Login;
